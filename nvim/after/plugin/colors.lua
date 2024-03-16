6b65e49ea1fa03d9f61f1188f645de983b4be882
tonyOehme <153957160+tonyOehme@users.noreply.github.com>
2024-03-16 05:54:32 +0100

    Add files via upload

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,18 @@
+vim.cmd("colorscheme onedark")
+require("onedarkpro").setup({
+  styles = {
+    types = "NONE",
+    methods = "NONE",
+    numbers = "NONE",
+    strings = "NONE",
+    comments = "NONE",
+    keywords = "bold",
+    constants = "NONE",
+    functions = "NONE",
+    operators = "NONE",
+    variables = "NONE",
+    parameters = "NONE",
+    conditionals = "NONE",
+    virtual_text = "NONE",
+  }
+})
