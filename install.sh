9dd3387c3c0a0953d6cd98a906aeec05c2601a52
Tony Andy Oehme <go98mub@mytum.de>
2024-03-23 00:18:04 +0100

    removed zen mode

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ defaults write .GlobalPreferences com.apple.mouse.scaling -1
 brew install --cask shottr
 brew install --cask google-chrome
 brew install git
+brew install tldr
 brew install --cask maccy
 brew install --cask iterm2
 brew install --cask alfred
