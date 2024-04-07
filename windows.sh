06ee0b5d2406d383eee72964c8af3611d0ef219e
Oehme, Tony Andy <tony-andy.oehme@adesso.de>
2024-04-07 16:59:04 +0200

    work on approuting

--- windows.sh
+++ windows.sh
@@ -2,5 +2,9 @@ irm christitus.com/win | iex
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install spotify
 choco install flow-launcher
-choco install googlechrome
 wsl --install -d
+choco install flow-launcher
+choco install spotify
+choco install powertoys
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"
+
