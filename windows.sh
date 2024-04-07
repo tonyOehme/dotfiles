b3afded78e556bacb7a74868f6d9e6b4ec1cdbbb
tonyOehme <153957160+tonyOehme@users.noreply.github.com>
2024-04-07 21:52:20 +0200

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,10 +1,9 @@
 irm christitus.com/win | iex
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install spotify
-choco install flow-launcher
-wsl --install -d
-choco install flow-launcher
-choco install spotify
-choco install powertoys
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))  
+wsl --install -d  
+winget install --id=Flow-Launcher.Flow-Launcher  -e   
+winget install --id=Spotify.Spotify  -e   
+winget install --id=JetBrains.IntelliJIDEA.Ultimate  -e  
+winget install --id tomlm.electron-outlook-com  -e  
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"  
 
