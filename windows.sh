f5d98a1ec889272b0415f30c62c8c83acf91d94d
Oehme, Tony Andy <tony-andy.oehme@adesso.de>
2024-04-10 01:13:17 +0200

    work on approuting

--- windows.sh
+++ windows.sh
@@ -1,9 +1,7 @@
+# need to install browser(s), peazip, code editors, discord with vencord, powertoys, windows terminal
 irm christitus.com/win | iex
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))  
-wsl --install -d  
-winget install --id=Flow-Launcher.Flow-Launcher  -e   
-winget install --id=Spotify.Spotify  -e   
-winget install --id=JetBrains.IntelliJIDEA.Ultimate  -e  
-winget install --id tomlm.electron-outlook-com  -e  
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"  
+wsl --install -d
+winget install --id=Flow-Launcher.Flow-Launcher  -e
+winget install --id=Spotify.Spotify  -e
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"
 
