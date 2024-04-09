f500955e9127b78ceaf4cdd2635eaa14f04c8dca
tonyOehme <153957160+tonyOehme@users.noreply.github.com>
2024-04-10 00:46:03 +0200

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -41,6 +41,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth 1 https://github.com/junegunn/fzf.git  ~/.fzf   
 ~/.fzf/install    
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 git clone https://github.com/tonyOehme/scripts.git ~/personal/scripts
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
